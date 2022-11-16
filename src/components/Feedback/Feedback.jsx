8340f60021d95b1bdfc33cfa4301792180477575
Fyl Andrii <fil.andrey91@gmail.com>
2022-11-16 16:19:03 +0200

    completes changes

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,3 +1,74 @@
+// Хуки=================================================================
+// вместо import {Component} импортируем {useState}*********************
+import { useState } from 'react';
+import css from './Feedback.module.css';
+import Statistics from 'components/Feedback/Statistics';
+import FeedbackOptions from 'components/Feedback/FeedbackOptions';
+import Section from 'components/Feedback/Section';
+import Notification from 'components/Feedback/Notification';
+
+export default function Feedback() {
+  // 0 в useState это базовое значение каждной из кнопок****************
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  // методы класса переписываем на функции
+  const addFeedbackIncrement = type => {
+    // устанавливаем switch - чтобы записать свойство в соответствуюющее свойство
+    switch (type) {
+      case 'good':
+        setGood(state => state + 1);
+        break;
+      case 'neutral':
+        setNeutral(state => state + 1);
+        break;
+      case 'bad':
+        setBad(state => state + 1);
+        break;
+      default:
+        throw new Error(`Unknows feedback type - ${type}`);
+    }
+  };
+
+  const countTotalFeedback = () => {
+    return good + neutral + bad;
+  };
+
+  const countPositiveFeedbackPercentage = () => {
+    return Number.isNaN(Math.round((good / countTotalFeedback()) * 100))
+      ? 0
+      : Math.round((good / countTotalFeedback()) * 100);
+  };
+
+  return (
+    <div className={css.Feedback__wrapper}>
+      <Section title={'Please live feedback'}>
+        <FeedbackOptions
+          options={['good', 'neutral', 'bad']}
+          onLeaveFeedback={addFeedbackIncrement}
+        />
+      </Section>
+
+      {countTotalFeedback() > 0 && (
+        <Section title={'Statistics'}>
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={countTotalFeedback()}
+            positivePercentage={countPositiveFeedbackPercentage()}
+          />
+        </Section>
+      )}
+      {countTotalFeedback() === 0 && (
+        <Notification message={'There is no feedback'} />
+      )}
+    </div>
+  );
+}
+
+// // Классовий Компонент VERSION=======================================
 // import React from 'react';
 // import css from './Feedback.module.css';
 // import Statistics from 'components/Feedback/Statistics';
@@ -5,7 +76,6 @@
 // import Section from 'components/Feedback/Section';
 // import Notification from 'components/Feedback/Notification';
 
-// // Классовий Компонент
 // class Feedback extends React.Component {
 //   state = {
 //     good: 0,
@@ -72,70 +142,3 @@
 // }
 
 // export default Feedback;
-// Хуки=================================================================
-// вместо import {Component} импортируем {useState}*********************
-import { useState } from 'react';
-import css from './Feedback.module.css';
-import Statistics from 'components/Feedback/Statistics';
-import FeedbackOptions from 'components/Feedback/FeedbackOptions';
-import Section from 'components/Feedback/Section';
-import Notification from 'components/Feedback/Notification';
-
-export default function Feedback() {
-  // 0 в useState это базовое значение каждной из кнопок****************
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
-
-  const addFeedbackIncrement = type => {
-    switch (type) {
-      case 'good':
-        setGood(state => state + 1);
-        break;
-      case 'neutral':
-        setNeutral(state => state + 1);
-        break;
-      case 'bad':
-        setBad(state => state + 1);
-        break;
-      default:
-        throw new Error(`Unknows feedback type - ${type}`);
-    }
-  };
-
-  const countTotalFeedback = () => {
-    return good + neutral + bad;
-  };
-
-  const countPositiveFeedbackPercentage = () => {
-    return Number.isNaN(Math.round((good / countTotalFeedback()) * 100))
-      ? 0
-      : Math.round((good / countTotalFeedback()) * 100);
-  };
-
-  return (
-    <div className={css.Feedback__wrapper}>
-      <Section title={'Please live feedback'}>
-        <FeedbackOptions
-          options={['good', 'neutral', 'bad']}
-          onLeaveFeedback={addFeedbackIncrement}
-        />
-      </Section>
-
-      {countTotalFeedback() > 0 && (
-        <Section title={'Statistics'}>
-          <Statistics
-            good={good}
-            neutral={neutral}
-            bad={bad}
-            total={countTotalFeedback()}
-            positivePercentage={countPositiveFeedbackPercentage()}
-          />
-        </Section>
-      )}
-      {countTotalFeedback() === 0 && (
-        <Notification message={'There is no feedback'} />
-      )}
-    </div>
-  );
-}
